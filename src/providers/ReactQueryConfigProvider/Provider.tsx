13d4bfcffe748743ee02a0caade970d7eb84304b
Ayham Hammami <ayhmhamame14@gmail.com>
2024-04-08 00:49:45 +0300

    feat (src/providers) : add the error handling and default options to the provider and get use of the handleResponse hook task_id : SCRUM-125

--- src/providers/ReactQueryConfigProvider/Provider.tsx
+++ src/providers/ReactQueryConfigProvider/Provider.tsx
@@ -1,5 +1,5 @@
-import decryptData from "@/helpers/decryptData";
-import encryptData from "@/helpers/encryptData";
+import { decryptData, encryptData, isRetryOnStatus } from "@/helpers";
+import { useHandleResponse, useAppDispatch } from "@/hooks";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
 import { persistQueryClient } from "react-query/persistQueryClient-experimental";
@@ -13,58 +13,38 @@ import { persistQueryClient } from "react-query/persistQueryClient-experimental"
  * @param {ReactNode} children - wrapped components
  */
 const CustomQueryClientProvider = ({ children }: any) => {
-  //   const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
+  const { handleError } = useHandleResponse();
   // useErrorHandler();
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
+        cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+        refetchOnMount: "always",
         refetchOnWindowFocus: false,
+        retry(failureCount, error: any) {
+          const RetryCount = 3;
+          if (
+            failureCount < RetryCount &&
+            isRetryOnStatus(error.response?.status)
+          )
+            return true;
+          else return false;
+        },
+        onError(err: any) {
+          console.error("error from provider", err);
+          handleError(err);
+        },
+      },
+      mutations: {
+        // onMutate() {},
+        onError(err: any, variables, context) {
+          handleError(err);
+        },
+        retry: 0,
       },
     },
   });
-
-  // {
-  //   defaultOptions: {
-  //     queries: {
-  //       // cacheTime: 1000 * 60 * 60 * 24, // 24 hours
-  //       refetchOnMount: "always",
-  //           refetchOnWindowFocus: false,
-  //           retry(failureCount, error: any) {
-  //         const RetryCount = 3;
-  //         if (
-  //             failureCount < RetryCount &&
-  //             isRetryOnStatus(error.response?.status)
-  //         )
-  //           return true;
-  //         else return false;
-  //       },
-  //       onError(err: any) {
-  //         dispatch(
-  //             UiSliceActions.SetError({
-  //               message: err.response?.data?.message ?? err.message,
-  //               code: err.response?.status ?? err.code,
-  //               navigate: err.response?.config?.method === "get",
-  //             })
-  //         );
-  //       },
-  //     },
-  //     mutations: {
-  //       onMutate() {
-  //         dispatch(UiSliceActions.SetError(null));
-  //       },
-  //       onError(err: any, variables, context) {
-  //         dispatch(
-  //             UiSliceActions.SetError({
-  //               message: err.response?.data?.message ?? err.message,
-  //               code: err.response?.status ?? err.code,
-  //               navigate: err.response?.config?.method === "get",
-  //             })
-  //         );
-  //       },
-  //       retry: 0,
-  //     },
-  //   },
-  // }
 
   const sessionStoragePersistor = createWebStoragePersistor({
     storage: window.sessionStorage,
